c14452f9e19d97153e095665f6bbb733ea59b58d
kjh5848~ <kjh5848@gmail.com>
2024-02-08 09:03:51 +0900

    240208 커밋 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,10 +1,8 @@
 package shop.mtcoding.blog.board;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RequiredArgsConstructor
 @Controller
